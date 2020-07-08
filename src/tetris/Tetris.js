7aec32caabed056eab4ac74a062ab35cc16157d1
Kevin Wilkinson <willkillson@gmail.com>
2020-07-08 01:37:25 -0700

    adds a shader to the game for testing

--- src/tetris/Tetris.js
+++ src/tetris/Tetris.js
@@ -37,6 +37,8 @@ class Tetris extends Component {
 
   }
 
+  
+
 
   
   componentDidMount() {
@@ -44,10 +46,97 @@ class Tetris extends Component {
     this.controls = new OrbitControls (this.camera, this.renderer.domElement);
 
     this.init();
+
+
+//attributes are per vertex
+
+    const vertexShader = 
+"attribute float vertexDisplacement;"+
+    "uniform float delta;"+
+    "varying float vOpacity;"+
+    "varying vec3 vUv;"+
+    
+"void main()" +
+ "{"+
+  "vUv = position;"+
+   "vOpacity = vertexDisplacement;"+
+    
+    "vec3 p = position;"+
+    
+        "p.x += sin(vertexDisplacement) * 10.0;"+
+        "p.y += cos(vertexDisplacement) * 50.0;"+
+    
+      "vec4 modelViewPosition = modelViewMatrix * vec4(p, 1.0);"+
+      "gl_Position = projectionMatrix * modelViewPosition;"+
+    "}";
+
+    const fragmentShader = 
+    "uniform float delta;"+
+    "varying float vOpacity;"+
+    "varying vec3 vUv;"+
+    
+    "void main() {"+
+    
+        "float r = 1.0 + cos(vUv.x * delta);"+
+        "float g = 0.5 + sin(delta) * 0.1;"+
+        "float b = 0.5 + cos(delta) * 0.1;"+
+        "vec3 rgb = vec3(r, g, b);"+
+    
+      "gl_FragColor = vec4(rgb, vOpacity);"+
+    "}";
+
+    var customUniforms = {
+      delta: {value: 0}
+  };
+    var material = new THREE.ShaderMaterial({
+        uniforms: customUniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    });
+    var geometry = new THREE.BoxBufferGeometry(100, 100, 100, 10, 10, 10);
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z = -1000;
+    mesh.position.x = -100;
+
+
+
+    var geometry2 = new THREE.SphereGeometry(50, 20, 20);
+    var mesh2 = new THREE.Mesh(geometry2, material);
+    mesh2.position.z = -1000;
+
+
+
+    var geometry3 = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+    var mesh3 = new THREE.Mesh(geometry3, material);
+    mesh3.rotation.x = -90 * Math.PI / 180;
+    mesh3.position.y = -100;
+    this.scene.add(mesh3);
+
+
+    //attribute
+    var vertexDisplacement = new Float32Array(geometry.attributes.position.count);
+
+    for (var i = 0; i < vertexDisplacement.length; i ++) {
+        vertexDisplacement[i] = Math.sin(i);
+    }
+
+    geometry.addAttribute('vertexDisplacement', new THREE.BufferAttribute(vertexDisplacement, 1));
+
+
+
+    let delta = 0;
     const animate = () => {
       this.currentPiece.update();
-    
 
+      delta += 0.1;
+        //uniform
+      mesh.material.uniforms.delta.value = 0.5 + Math.sin(delta) * 0.5;
+
+      //attribute
+      for (var i = 0; i < vertexDisplacement.length; i ++) {
+          vertexDisplacement[i] = 0.5 + Math.sin(i + delta) * 0.25;
+      }
+      mesh.geometry.attributes.vertexDisplacement.needsUpdate = true;
     
       this.renderer.render( this.scene, this.camera );
       requestAnimationFrame( animate );
